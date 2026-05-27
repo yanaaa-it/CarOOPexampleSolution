0a140ec959a41f717abcd03b1f3663f7376c1889
yanaaa-it <yaninalikemiki@gmail.com>
2026-05-27 17:26:33 +0300

    Update main2.cpp

---
+++ lesson64mainSolution/main2.cpp
@@ -0,0 +1,20 @@
+//#include "Student.h"
+//
+//int main() {
+//        Student* alex = new Student(123, "Alex", 15, 9, 'A', 10, true);
+//        Student* vlad;
+//
+//        vlad = alex;
+//
+//        cout << "Before: ";
+//        cout << alex->toString() << endl;
+//        cout << vlad->toString() << endl;
+//
+//        alex->id = 100;
+//
+//        cout << "After: ";
+//        cout << alex->toString() << endl;
+//        cout << vlad->toString() << endl;
+//
+//        return 0;
+//}
