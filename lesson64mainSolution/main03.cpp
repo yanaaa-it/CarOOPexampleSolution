aed7351d858af2e63655f708ba4a11a425a780ad
yanaaa-it <yaninalikemiki@gmail.com>
2026-05-27 17:22:23 +0300

    Update main03.cpp

---
+++ lesson64mainSolution/main03.cpp
@@ -0,0 +1,17 @@
+//#include "Car.h"
+//
+//int main() {
+//        /*int number{ };
+//        cout << "number = " << number << endl;
+//
+//        int* array = new int[3] {1, 1000};
+//        cout << array[0] << ", " << array[1] << ", " << array[2] << endl;
+//
+//        Car alex{10, "Alex", 14, 9, 'A', true};
+//        delete[] array;*/
+//
+//        Car* alex = new Car();
+//        cout << alex->getAge() << endl;
+//
+//        return 0;
+//}
